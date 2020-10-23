55fb2a53435d2018cafe3bb000ab22181e29c34d
Sara Cemal <saracemal@gmail.com>
2020-10-23 04:25:00 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,17 +1,19 @@
 def find_min_in_nested_arrays(src)
-  outer_results = []
-row_index = 0
-while row_index < src.count do
-  element_index = 0
-  smallest_element = ""
-  while element_index < src[row_index].count do
-      if src[row_index][element_index].length > longest_string_element.length
-      longest_string_element = spice_rack[row_index][element_index]
+outer_results = []
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    inner_results = []
+    lowest_value = 100
+    while element_index < src[row_index].count do
+      if src[row_index][element_index] < lowest_value
+        lowest_value = src[row_index][element_index]
+      end
+      element_index += 1
     end
-    element_index += 1
+    outer_results << lowest_value
+    row_index += 1
   end
-    outer_results << longest_string_element
-  row_index += 1
-end
- 
+  outer_results
+
 end
